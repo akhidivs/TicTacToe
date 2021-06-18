7f3f577bbea5136b5d981b198382d33c0802b26a
Akhilesh Mishra <akhidivs@gmail.com>
2021-06-18 15:51:29 +0530

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Akhilesh Mishra on 18/06/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
